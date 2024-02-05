9773ff80b8cd01cbdc88138009147c892aea0707
AnilBerkanTorun <159058312+AnilBerkanTorun@users.noreply.github.com>
2024-02-06 02:38:59 +0300

    Add files via upload

---
+++ jira/jira.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+
+
+int yas,tecrube;
+
+
+printf("yas gir,%d");
+
+printf("tecrube gir,%d");
+
+
+scanf("%d",&yas);
+
+scanf("%d",&tecrube);
+
+
+if(35<yas<45&&tecrube>10){
+
+
+
+  
+  printf("atlassian sirketinde ise alindiniz");  
+  
+  
+  }
+  
+  
+else {
+
+  printf("atlassian sirketinde ise alinmadiniz");
+  
+  
+  
+  }
+	
+
+
+
+  
+return 0;
+
+
+}
+
